aae37e7332ed146ad8fdfde1fd8d8b6b991e61ad
tatiana2404 <74406991+tatiana2404@users.noreply.github.com>
2020-11-19 20:14:54 +0200

    Add files via upload

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -6,21 +6,30 @@ public class Board {
     private Shape[] figures=new Shape[4];
     public Board(){  }
     public void addShape(Shape fig,int n){
-        if(figures[n]==null) {
-            figures[n] = fig;
-            System.out.println("Фигура успешно добавлена");
+        if(n>=0 && n<=3){
+            if(figures[n]==null) {
+                figures[n] = fig;
+                System.out.println("Фигура успешно добавлена");
+            }
+            else {
+                System.out.println("Место "+n+" уже занято");
+            }
         }
         else {
-            System.out.println("Место "+n+" уже занято");
+                System.out.println("Введите коректный номер");
         }
     }
     public void deleteShape(int n){
-        if (figures[n]!=null) {
-            figures[n] = null;
-            System.out.println("Фигура успешно удалена");
+        if(n>=0 && n<=3) {
+            if (figures[n] != null) {
+                figures[n] = null;
+                System.out.println("Фигура успешно удалена");
+            } else {
+                System.out.println("Это место и так пусто");
+            }
         }
-        else{
-            System.out.println("Это место и так пусто");
+        else {
+            System.out.println("Введите корректый номер");
         }
     }
     public double areaFigures(){
